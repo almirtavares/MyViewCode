ecfb3829f58bfc1d346cc7b2f43d9f26aabcf772
Almir Tavares <eng.almirtavares@gmail.com>
2021-10-18 21:05:39 -0300

    Initial Commit

---
+++ MyViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyViewCode
+//
+//  Created by almir.tavares on 18/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
